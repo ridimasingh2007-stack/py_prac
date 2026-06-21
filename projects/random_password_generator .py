54ea8d1dfdb2a7583809e0854df03e4edd775989
Ridima Singh <ridimasingh2007@gmail.com>
2026-06-21 20:23:49 +0530

    Make password length configurable via user input

--- projects/random_password_generator .py
+++ projects/random_password_generator .py
@@ -1,8 +1,9 @@
 import random
 import string
 
-pass_len = 8
+pass_len = int(input("Enter password length: "))
 charValues = string.ascii_letters + string.digits + string.punctuation
 
 password = "".join([random.choice(charValues)for i in range(pass_len)])
 print('Your random password is:', password)
+
