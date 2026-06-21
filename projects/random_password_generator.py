8ba0370f19769ee22214d6d49672e6aa7cfdfd23
Ridima Singh <ridimasingh2007@gmail.com>
2026-06-21 20:25:55 +0530

    Delete projects/random_password_generator.py

--- projects/random_password_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-pass_len = int(input("Enter password length: "))
-charValues = string.ascii_letters + string.digits + string.punctuation
-
-password = "".join([random.choice(charValues)for i in range(pass_len)])
-print('Your random password is:', password)
